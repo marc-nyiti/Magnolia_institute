4d526c3f1686205c7204f2212213a32d54990607
Marc Nyiti <dripcity@gmail.com>
2020-10-20 19:01:56 -0700

    Updates

---
+++ precache-manifest.7fce5ca1d6b08997101c39f55e70dfef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "234baa564aca514dafc1",
+    "url": "/Magnolia_institute/static/css/main.aea57417.chunk.css"
+  },
+  {
+    "revision": "234baa564aca514dafc1",
+    "url": "/Magnolia_institute/static/js/main.234baa56.chunk.js"
+  },
+  {
+    "revision": "62dfdb4a41aa31ad4878",
+    "url": "/Magnolia_institute/static/css/1.053dc685.chunk.css"
+  },
+  {
+    "revision": "62dfdb4a41aa31ad4878",
+    "url": "/Magnolia_institute/static/js/1.62dfdb4a.chunk.js"
+  },
+  {
+    "revision": "8171236414e0996b0893",
+    "url": "/Magnolia_institute/static/js/runtime~main.81712364.js"
+  },
+  {
+    "revision": "fa8a5cdefd42e81f72b6b8f917192210",
+    "url": "/Magnolia_institute/static/media/dataviz.fa8a5cde.gif"
+  },
+  {
+    "revision": "75bbae50d18d20c60e59da0197554643",
+    "url": "/Magnolia_institute/static/media/topBanner.75bbae50.png"
+  },
+  {
+    "revision": "0affcb09e874b622dca1c1a941960ff8",
+    "url": "/Magnolia_institute/index.html"
+  }
+];
